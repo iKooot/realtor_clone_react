187d66286cf413ca18b204d0c31a8adc262ffb6b
Serhii Savchuk <serhii.savchuk@altran.com>
2023-07-29 23:53:28 +0300

    compleate the oauth functionality

--- src/components/OAth.jsx
+++ src/components/OAth.jsx
@@ -1,6 +1,41 @@
 import { FcGoogle } from "react-icons/fc";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export const OAth = (props) => {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async (event) => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+
+      const user = result.user;
+
+      // check for the user is in db
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+      toast.success(`${user.displayName} successfully authorized`);
+    } catch (error) {
+      console.error(error);
+      toast.error("Could not authorize with Google");
+    }
+  };
   return (
     <button
       className="
@@ -23,6 +58,8 @@ export const OAth = (props) => {
         duration-150
         ease-in-out
         rounded"
+      onClick={onGoogleClick}
+      type="button"
     >
       <FcGoogle className="text-xl bg-white rounded-full mr-2" />
       Continue with Google
