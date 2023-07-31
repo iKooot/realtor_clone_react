daaaa7e18de3aa01236524eb0f6d7792eced9af9
Serhii Savchuk <serhii.savchuk@altran.com>
2023-07-31 12:08:51 +0300

    fixed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANoNZvOwpd4BZsDRkrOT7lyPmFOqXcXO0",
-  authDomain: "realtor-clone-react-c6958.firebaseapp.com",
-  projectId: "realtor-clone-react-c6958",
-  storageBucket: "realtor-clone-react-c6958.appspot.com",
-  messagingSenderId: "685346212868",
-  appId: "1:685346212868:web:5faa8af577d1c6f43cf951",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
